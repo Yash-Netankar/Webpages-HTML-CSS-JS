0da5090198c568d4531fc4d15acebf5603e3019f
Yash-Netankar <60968654+Yash-Netankar@users.noreply.github.com>
2020-06-21 15:00:04 +0530

    Add files via upload
    Calculator using HTML CSS and 2%JAVASCRIPT

---
+++ calculator/js.js
@@ -0,0 +1,8 @@
+function fun(){
+    
+        let btn = document.getElementById('para').innerHTML="Result Printed";
+}
+function fun2(){
+    
+    let btn = document.getElementById('para').innerHTML="";
+}
